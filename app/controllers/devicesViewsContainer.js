9289c5436d8a133d4dcbb9a285c424b30f5e85e5
kgividen <register@gividen.com>
2014-11-01 09:15:13 -0600

    Added scenes to swip View.

--- app/controllers/devicesViewsContainer.js
+++ app/controllers/devicesViewsContainer.js
@@ -1,2 +1,3 @@
 $.scrollableView.addView(Alloy.createController("devices", {viewId: VIEW_ID_FAVORITES, sortBy: "showInFavoritesViewSortId"}).getView());
 $.scrollableView.addView(Alloy.createController("devices", {viewId: VIEW_ID_LIGHTS, sortBy: "showInLightingViewSortId"}).getView());
+$.scrollableView.addView(Alloy.createController("devices", {viewId: VIEW_ID_SCENES, sortBy: "showInLightingViewSortId"}).getView());
