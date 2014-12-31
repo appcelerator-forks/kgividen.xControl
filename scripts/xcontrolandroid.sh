727875537d64069eedeee500188bfa6f48f1c4f3
kgividen <register@gividen.com>
2014-12-31 10:55:49 -0700

    Added line for different emulator

--- scripts/xcontrolandroid.sh
+++ scripts/xcontrolandroid.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd /Users/kgividen/Data/Titanium/xControl
-ti build --platform android --device-id "Samsung Galaxy S4 - 4.3 - API 18 - 1080x1920"
+#ti build --platform android --device-id "Samsung Galaxy S4 - 4.3 - API 18 - 1080x1920"
+ti build --platform android --device-id "Samsung Galaxy S4 - 4.2.2 - API 17 - 1080x1920"
 
