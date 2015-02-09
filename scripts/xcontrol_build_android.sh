a8f9434cc470dd5a1ed13d52b9a55b2e75c8c8f8
kgividen <register@gividen.com>
2015-02-09 14:35:49 -0700

    Use the correct keystore

--- scripts/xcontrol_build_android.sh
+++ scripts/xcontrol_build_android.sh
@@ -5,7 +5,7 @@ rm $XCONTROL_PATH/delivery/*.apk
 ti clean
 ti build -p android -T dist-playstore -K /Users/kgividen/.android/key/$1Keystore -P $2 -L xControl -O $XCONTROL_PATH/delivery/
 mv $XCONTROL_PATH/delivery/$1.apk $XCONTROL_PATH/delivery/$1Unsigned.apk
-jarsigner -verbose -storepass $2 -sigalg MD5withRSA -digestalg SHA1 -keystore ~/.android/key/$1Keystore $XCONTROL_PATH/delivery/$1Unsigned.apk $1
+jarsigner -verbose -storepass $2 -sigalg MD5withRSA -digestalg SHA1 -keystore ~/.android/key/$1Key $XCONTROL_PATH/delivery/$1Unsigned.apk $1
 jarsigner -verify -verbose -certs $XCONTROL_PATH/delivery/$1Unsigned.apk
 /Users/kgividen/apps/android/tools/zipalign -v 4 $XCONTROL_PATH/delivery/$1Unsigned.apk $XCONTROL_PATH/delivery/$1.apk
 rm $XCONTROL_PATH/delivery/$1Unsigned.apk
