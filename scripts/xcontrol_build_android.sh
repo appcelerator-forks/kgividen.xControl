187f7296f4276306108a004a992e28affdd34fa8
kgividen <register@gividen.com>
2016-02-01 18:55:19 -0700

    Made it use the correct building keystore and revved version.

--- scripts/xcontrol_build_android.sh
+++ scripts/xcontrol_build_android.sh
@@ -3,7 +3,7 @@ export XCONTROL_PATH=/Users/kgividen/Data/Titanium/xControl
 cd $XCONTROL_PATH
 rm $XCONTROL_PATH/delivery/*.apk
 appc ti clean
-appc ti build -p android -T dist-playstore -K /Users/kgividen/.android/key/$1Keystore -P $2 -L xControl -O $XCONTROL_PATH/delivery/
+appc ti build -p android -T dist-playstore -K /Users/kgividen/.android/key/$1Key -P $2 -L xControl -O $XCONTROL_PATH/delivery/
 mv $XCONTROL_PATH/delivery/$1.apk $XCONTROL_PATH/delivery/$1Unsigned.apk
 jarsigner -verbose -storepass $2 -sigalg MD5withRSA -digestalg SHA1 -keystore ~/.android/key/$1Key $XCONTROL_PATH/delivery/$1Unsigned.apk $1
 jarsigner -verify -verbose -certs $XCONTROL_PATH/delivery/$1Unsigned.apk
