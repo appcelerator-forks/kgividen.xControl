062dbb471c0c523afa1d5813ef8f370221c2f124
kgividen <register@gividen.com>
2016-04-08 22:03:43 -0600

    cleanup some comments and log msgs

--- app/controllers/options.js
+++ app/controllers/options.js
@@ -1,13 +1,10 @@
 var args = arguments[0] || {};
 
-Ti.API.info("swipeViewDisabled:" + Titanium.App.Properties.getInt("swipeViewDisabled"));
-
 var swipeViewDisabled = (Titanium.App.Properties.getInt("swipeViewDisabled")) ? true : false;
 
 $.switchDisableViewSwipe.setValue(swipeViewDisabled);
 
 $.switchDisableViewSwipe.addEventListener('change',function(e) {
-	Ti.API.info("e.value: " + e.value);
 	Titanium.App.Properties.setInt("swipeViewDisabled",e.value);
 });
 
