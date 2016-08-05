72949e9e9d8a225e1e81ceee0f01a64db4a61faf
kgividen <register@gividen.com>
2016-08-05 11:36:57 -0600

    disable debug for now. ignore some files

--- app/controllers/debug.js
+++ app/controllers/debug.js
@@ -1,55 +1,79 @@
-var fakeData = require("/data/fakeData");
+// var fakeData = require("/data/fakeData");
+// 
+// $.printDeviceBtn.addEventListener('click', function () {
+    // var blah = $.device;
+    // blah.fetch();
+    // Ti.API.info("device: " + JSON.stringify(blah));
+    // $.output.value = JSON.stringify(blah);
+// });
+// 
+// $.closeBtn.addEventListener('click', function () {
+    // $.win.close();
+// });
+// 
+// function printFolderInViewTable () {
+ 	// var blah = Alloy.Collections.folderInView;
+    // blah.fetch();
+    // Ti.API.info("view: " + JSON.stringify(blah));
+    // $.output.value = JSON.stringify(blah);		
+// }
+// 
+// function printDeviceInFolderTable () {
+ 	// var blah = Alloy.Collections.deviceInFolder;
+    // blah.fetch();
+    // Ti.API.info("view: " + JSON.stringify(blah));
+    // $.output.value = JSON.stringify(blah);		
+// }
+// 
+// function createFakeData () {
+	// fakeData.createFakeData();
+// }
+// 
+// function deleteFakeData () {
+	// fakeData.deleteFakeData();
+// }
+// 
+// function createFakeDevices () {
+	// fakeData.createFakeDevices();
+// }
+// 
+// function createFakeFolders () {
+	// fakeData.createFakeFolders();
+// }
 
-$.printDeviceBtn.addEventListener('click', function () {
-    var blah = $.device;
-    blah.fetch();
-    Ti.API.info("device: " + JSON.stringify(blah));
-    $.output.value = JSON.stringify(blah);
+
+// $.btn.onClick(function (e) {
+	// Ti.API.info("onClick btn1 E: " + JSON.stringify(e));
+// });
+// 
+// $.btn.onTouchEnd(function (e) {
+	// Ti.API.info("touchend E: " + JSON.stringify(e));
+// });
+// 
+// $.win.addEventListener("open", function() {
+    // $.btn.setBtnValue(10);
+// });
+
+Ti.API.info("Debug window opened!");
+var websocket = require('net.iamyellow.tiws').createWS();
+
+websocket.addEventListener('open', function () {
+	Ti.API.info('websocket opened');
+	websocket.send("blah");
 });
 
-$.closeBtn.addEventListener('click', function () {
-    $.win.close();
+websocket.addEventListener('close', function (ev) {
+	Ti.API.info('websocket closed');
 });
 
-function printFolderInViewTable () {
- 	var blah = Alloy.Collections.folderInView;
-    blah.fetch();
-    Ti.API.info("view: " + JSON.stringify(blah));
-    $.output.value = JSON.stringify(blah);		
-}
-
-function printDeviceInFolderTable () {
- 	var blah = Alloy.Collections.deviceInFolder;
-    blah.fetch();
-    Ti.API.info("view: " + JSON.stringify(blah));
-    $.output.value = JSON.stringify(blah);		
-}
-
-function createFakeData () {
-	fakeData.createFakeData();
-}
-
-function deleteFakeData () {
-	fakeData.deleteFakeData();
-}
-
-function createFakeDevices () {
-	fakeData.createFakeDevices();
-}
-
-function createFakeFolders () {
-	fakeData.createFakeFolders();
-}
-
-
-$.btn.onClick(function (e) {
-	Ti.API.info("onClick btn1 E: " + JSON.stringify(e));
+websocket.addEventListener('error', function (ev) {
+	// ev.error contains error description, if any
+	Ti.API.info(ev.error);
+});
+ 
+websocket.addEventListener('message', function (ev) {
+	// ev.data contains message data
+	Ti.API.log(ev.data);
 });
 
-$.btn.onTouchEnd(function (e) {
-	Ti.API.info("touchend E: " + JSON.stringify(e));
-});
-
-$.win.addEventListener("open", function() {
-    $.btn.setBtnValue(10);
-});
+websocket.open('ws://localhost:8001');
